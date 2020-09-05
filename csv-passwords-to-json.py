4390606c172cdbb97deeb755045491be018cfeec
Antoni Marzec <marzeqmarzeq@gmail.com>
2020-09-05 14:46:06 +0200

    Added command line arguments

--- csv-passwords-to-json.py
+++ csv-passwords-to-json.py
@@ -1,9 +1,22 @@
 import json
+import sys
 
-with open("out.json", "r+") as jsonfile:
+sys.argv.pop(0)
+try:
+    inpt = sys.argv[0]
+except IndexError:
+    print("Provide the input file")
+    quit()
+try:
+    outpt = sys.argv[1]
+except IndexError:
+    print("Provide the output file")
+    quit()
+
+with open(outpt, "r+") as jsonfile: # noqa
     output = {}
     jsonfile.write("")
-    with open("Chrome Passwords.csv", "r") as csvfile:
+    with open(inpt, "r") as csvfile: # noqa
         passes = csvfile.read().split("\n")
         passes.pop(0)
         for pas in passes:
@@ -16,5 +29,5 @@ with open("out.json", "r+") as jsonfile:
                 try:
                     output[pas[0]].append({"username": pas[2], "password": pas[3]})
                 except IndexError as e:
-                    print(pas)
+                    print(e, pas)
         json.dump(output, jsonfile, indent=4)
